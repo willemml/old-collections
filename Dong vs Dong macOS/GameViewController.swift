817be49f88bb029411acb3430f9bca83b47cf202
willemml <willem@leit.so>
2020-11-21 22:31:16 -0800

    Initial Commit

---
+++ Dong vs Dong macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Dong vs Dong macOS
+//
+//  Created by Willem Leitso on 2020-11-21.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
